4e96e4493fe08c3ea7530610606101442431bc4f
Archit Verma <architv07@gmail.com>
2015-10-12 09:35:19 +0000

    fetch licenses or exit

--- butler.py
+++ butler.py
@@ -1,14 +1,28 @@
 import os
 import sys
 
+import click
+
 import requests
 
-BASE_URL = ""
+BASE_URL = "https://api.github.com"
 _ROOT = os.path.abspath(os.path.dirname(__file__))
 
 
+def get_licences():
+  req = requests.get(BASE_URL)
+
+  if req.status_code = requests.status.ok:
+    licenses = req.json()
+  else:
+    click.secho("Couldn't get the data", fg="red", bold=True)
+    click.secho("Exiting...", fg="red", bold=True)
+    sys.exit()
+
 def main():
+  """ butler helps you manage and add license from the command line """
 	pass
+  
 
 if __name__ == '__main__':
 	main()
