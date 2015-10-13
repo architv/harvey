80345a4e4c19968898952e4e707ef98d88f3bec3
Archit Verma <architv07@gmail.com>
2015-10-13 11:09:55 +0000

    added option to view the licenses

--- butler.py
+++ butler.py
@@ -5,23 +5,41 @@ import click
 
 import requests
 
-BASE_URL = "https://api.github.com"
-_ROOT = os.path.abspath(os.path.dirname(__file__))
+BASE_URL = "https://api.github.com/licenses"
+_HEADERS = {'Accept': 'application/vnd.github.drax-preview+json'}
 
 
 def get_licences():
-  req = requests.get(BASE_URL)
+  req = requests.get(BASE_URL, headers=_HEADERS)
 
   if req.status_code = requests.status.ok:
-    licenses = req.json()
+    return req.json()
   else:
     click.secho("Couldn't get the data", fg="red", bold=True)
     click.secho("Exiting...", fg="red", bold=True)
     sys.exit()
 
+
+def _get_licences():
+  """ lists all the licenses on command line """
+  licenses = {}
+  with open('licenses.json', 'r') as f:
+    licenses = json.loads(f.read())
+  
+  for license in licenses:
+    print "{license_name} [{license_code}]".format(license_name=licenses[license], license_code=license)
+
+
 def main():
   """ butler helps you manage and add license from the command line """
-	pass
+	arguments = docopt(__doc__, version=__version__)
+
+  if arguments['ls'] or arguments['list']:
+      print(_get_filenames())
+  elif arguments['NAME']:
+      print(_handle_gitignores(arguments['NAME']))
+  else:
+      print(__doc__)
   
 
 if __name__ == '__main__':
