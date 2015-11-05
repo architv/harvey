2e8571d544fd5d5dc19b3d13fe6cf4f0b52bad31
Archit Verma <architv07@gmail.com>
2015-11-05 22:48:25 +0530

    Revert b910b25e50067e163a702d223e5dc02f393bb688

--- harvey/get_tldr.py
+++ harvey/get_tldr.py
@@ -9,63 +9,61 @@ BASE_URL = "https://api.github.com"
 _HEADERS = {'Accept': 'application/vnd.github.drax-preview+json'}
 
 with open('licences_tldr.json', 'r') as f:
-    RESOURCES = json.loads(f.read())
+  RESOURCES = json.loads(f.read())
 
 
 def get_summary(content):
-    """Gets the summary of a license from tldrlegal.com"""
-    soup = BeautifulSoup(content, "html.parser")
-    summary = soup.find_all(attrs={
-        'class': re.compile(r".*\bsummary-content\b.*")})
+  """Gets the summary of a license from tldrlegal.com"""
+  soup = BeautifulSoup(content, "html.parser")
+  summary = soup.find_all(attrs={'class': re.compile(r".*\bsummary-content\b.*")})
 
-    if summary[0].p.string is None:
-        return summary[0].p.p.getText()
-    return summary[0].p.getText()
+  if summary[0].p.string == None:
+    return summary[0].p.p.getText()
+  return summary[0].p.getText()
 
 
 def get_rules(license):
-    """Gets can, cannot and must rules from github license API"""
+  """Gets can, cannot and must rules from github license API"""
 
-    can = []
-    cannot = []
-    must = []
-    req = requests.get("{base_url}/licenses/{license}".format(
-                       base_url=BASE_URL, license=license),
-                       headers=_HEADERS)
+  can = []
+  cannot = []
+  must = []
+  req = requests.get("{base_url}/licenses/{license}".format(base_url=BASE_URL, license=license), 
+    headers=_HEADERS)
 
-    if req.status_code == requests.codes.ok:
-        data = req.json()
-        can = data["permitted"]
-        cannot = data["forbidden"]
-        must = data["required"]
+  if req.status_code == requests.codes.ok:
+    data = req.json()
+    can = data["permitted"]
+    cannot = data["forbidden"]
+    must = data["required"]
 
-    return can, cannot, must
+  return can, cannot, must
 
 
 def main():
-    """Gets all the license information and stores it in json format"""
+  """Gets all the license information and stores it in json format"""
 
-    all_summary = {}
+  all_summary = {}
 
-    for license in RESOURCES:
+  for license in RESOURCES:
 
-        req = requests.get(RESOURCES[license])
+    req = requests.get(RESOURCES[license])
 
-        if req.status_code == requests.codes.ok:
-            summary = get_summary(req.text)
-            can, cannot, must = get_rules(license)
+    if req.status_code == requests.codes.ok:
+      summary = get_summary(req.text)
+      can, cannot, must = get_rules(license)
 
-            all_summary[license] = {
-                "summary": summary,
-                "source": RESOURCES[license],
-                "can": can,
-                "cannot": cannot,
-                "must": must
-            }
+      all_summary[license] = {
+        "summary": summary,
+        "source": RESOURCES[license],
+        "can": can,
+        "cannot": cannot,
+        "must": must
+      }
 
-    with open('summary.json', 'w+') as f:
-        f.write(json.dumps(all_summary, indent=4))
+  with open('summary.json', 'w+') as f:
+    f.write(json.dumps(all_summary, indent=4))
 
 
 if __name__ == '__main__':
-    main()
+  main()
--- setup.py
+++ setup.py
@@ -6,32 +6,32 @@ from setuptools import setup, find_packages
 
 # if you are not using vagrant, just delete os.link directly,
 # The hard link only saves a little disk space, so you should not care
-if os.environ.get('USER', '') == 'vagrant':
-    del os.link
+if os.environ.get('USER','') == 'vagrant':
+  del os.link
 
 
 setup(
-    name='harvey',
-    version='0.0.4.4',
-    description='harvey helps you manage and choose license from command line',
-    long_description=open('README.rst').read(),
-    author='Archit Verma',
-    author_email='architv07@gmail.com',
-    license='MIT',
-    keywords=['license', 'legal', 'github', 'command line', 'cli'],
-    url='https://github.com/architv/harvey',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    package_data={
-        'harvey': ['*.json'],
-    },
-    install_requires=[
-        'docopt>=0.6.2',
-        'requests==2.8.0',
-        'colorama==0.3.3'
+  name='harvey',
+  version='0.0.4.4',
+  description='harvey helps you manage and choose license from command line',
+  long_description=open('README.rst').read(),
+  author='Archit Verma',
+  author_email='architv07@gmail.com',
+  license='MIT',
+  keywords=['license', 'legal', 'github', 'command line', 'cli'],
+  url='https://github.com/architv/harvey',
+  packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+  package_data={
+    'harvey': ['*.json'],
+  },
+  install_requires=[
+    'docopt>=0.6.2',
+    'requests==2.8.0',
+    'colorama==0.3.3'
+  ],
+  entry_points={
+    'console_scripts': [
+      'harvey = harvey.harvey:main'
     ],
-    entry_points={
-        'console_scripts': [
-            'harvey = harvey.harvey:main'
-        ],
-    }
+  }
 )
